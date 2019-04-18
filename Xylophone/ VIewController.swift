0c096f8807ad9e29d1649b9164959e42a8b85f54
Jodi Smit <jodi@nebulahome.com>
2019-04-18 15:24:33 -0400

    Updated passing to function.

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,7 +12,6 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
 	
 	var player: AVAudioPlayer?
-	var selectedSoundFileName = ""
 	let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
@@ -23,13 +22,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBAction func notePressed(_ sender: UIButton) {
 		
-		selectedSoundFileName = soundArray[sender.tag - 1]
-		playSound()
+		let selectedSoundFileName = soundArray[sender.tag - 1]
+		playSound(for: selectedSoundFileName)
 
     }
 	
-	func playSound() {
-		let url = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")!
+	func playSound(for soundFile: String) {
+		let url = Bundle.main.url(forResource: soundFile, withExtension: "wav")!
 		
 		do {
 			player = try AVAudioPlayer(contentsOf: url)

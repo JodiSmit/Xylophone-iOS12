d61b9e7378bd662dec78f46819a1918f75d89328
Jodi Smit <jodi@nebulahome.com>
2019-04-18 14:33:51 -0400

    Updated to pay sounds

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
-    
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+	
+	var player: AVAudioPlayer?
+	var selectedSoundFileName = ""
+	let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +22,24 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
-    }
-    
-  
+		
+		selectedSoundFileName = soundArray[sender.tag - 1]
+		playSound()
 
+    }
+	
+	func playSound() {
+		let url = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")!
+		
+		do {
+			player = try AVAudioPlayer(contentsOf: url)
+			guard let player = player else { return }
+			
+			player.prepareToPlay()
+			player.play()
+		} catch let error as NSError {
+			print(error)
+		}
+	}
 }
 
